dd5be069970e64ca260c04c90f7bc52f5563cbb1
Freddie Nelson <freddie0208@hotmail.com>
2020-11-03 19:50:04 +0000

    setup project

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,23 @@
 module.exports = {
   root: true,
+
   env: {
     node: true
   },
-  extends: ["plugin:vue/vue3-essential", "eslint:recommended", "@vue/prettier"],
+
   parserOptions: {
-    parser: "babel-eslint"
+    parser: "@typescript-eslint/parser"
   },
+
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
-  }
+  },
+
+  extends: [
+    "plugin:vue/vue3-essential",
+    "eslint:recommended",
+    "@vue/prettier",
+    "@vue/typescript"
+  ]
 };
--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,13 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <h1>vue3-slider</h1>
 </template>
 
-<script>
-import HelloWorld from "./components/HelloWorld.vue";
+<script lang="ts">
+import { defineComponent } from "vue";
 
-export default {
-  name: "App",
-  components: {
-    HelloWorld
-  }
-};
+export default defineComponent({
+  name: "App"
+});
 </script>
 
 <style>
